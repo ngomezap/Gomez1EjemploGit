3e1522d77ffab643d1cdb36270bab6cd4121214f
dam111 <dam111@36-13.aglinformatica.es>
2022-04-28 18:13:21 +0200

    Hola Mundo!

---
+++ src/gomez1ejemplogit/Gomez1EjemploGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gomez1ejemplogit;
+
+/**
+ *
+ * @author dam111
+ */
+public class Gomez1EjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo!");
+    }
+    
+}
