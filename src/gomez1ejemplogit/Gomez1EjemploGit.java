9c6d0e4874ec4f59b889cea816c79b167436f868
dam111 <dam111@36-13.aglinformatica.es>
2022-04-28 18:13:47 +0200

    Version 2

--- src/gomez1ejemplogit/Gomez1EjemploGit.java
+++ src/gomez1ejemplogit/Gomez1EjemploGit.java
@@ -17,6 +17,7 @@ public class Gomez1EjemploGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo!");
+        System.out.println("version 2");
     }
     
 }
